dab39d42ca86df908fc5b60953389233c555dd48
Lucas Vieira <lucasltv@gmail.com>
2023-03-25 08:51:10 -0300

    Custom headers

--- src/helpers/fetch.helper.ts
+++ src/helpers/fetch.helper.ts
@@ -1,10 +1,19 @@
-export async function fetchData(url: string): Promise<string> {
+export async function fetchData(
+  url: string,
+  customHeaders?: Record<string, string>
+): Promise<string> {
   const headers = new Headers();
 
   headers.set(
     'User-Agent',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
   );
+
+  if (!!customHeaders) {
+    Object.keys(customHeaders).forEach(key => {
+      headers.set(key, customHeaders[key]);
+    });
+  }
 
   const res = await fetch(url, {
     method: 'GET',
